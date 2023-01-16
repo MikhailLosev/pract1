0a7f2a59e1533bcb045d03756a1f9db5c04eaa7c
MikhailLosev <115175824+MikhailLosev@users.noreply.github.com>
2023-01-16 20:56:51 +0300

    Update p2.py

--- pract3/p2.py
+++ pract3/p2.py
@@ -1,14 +1,22 @@
 import itertools
-def func(list:list)->set:
+
+
+def func(list_: list) -> set:
     list2 = []
-    res = itertools.permutations(list)
+    res = itertools.permutations(list_)
     for i in res:
         list2.append(i)
     return set(list2)
 
+
 def main():
-    list = [1,1,2]
-    
-    print(func(list))
+    list_ = str(input())
+    list_ = list_.strip()
+    list_ = list_.split()
+    list_ = [int(x) for x in list_]
+
+    print(func(list_))
+
+
 if __name__ == "__main__":
     main()
