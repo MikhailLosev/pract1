f2690f1381c36bd964aa59953aa6b962ee576d32
MikhailLosev <115175824+MikhailLosev@users.noreply.github.com>
2023-01-16 20:54:20 +0300

    Update p.py

--- lab2/p.py
+++ lab2/p.py
@@ -1,124 +1,127 @@
-def main():
-    from datetime import datetime
-    import time
+from datetime import datetime
+import time
 
 
-    def cell_is_free(x, y, old_fig_arr, new_fig_arr):
-        return (x, y) not in (old_fig_arr + new_fig_arr)
+def cell_is_free(x: int, y: int, old_fig_arr: list, new_fig_arr: list) -> bool:
+    return (x, y) not in (old_fig_arr + new_fig_arr)
 
 
-    def cell_is_attacked(x, y, old_fig_arr, new_fig_arr):
-        attack_mod_delta_x = (0, 1, 2, 3)
-        attack_mod_delta_y = (0, 1, 2, 3)
+def cell_is_attacked(x: int, y: int, old_fig_arr: list, new_fig_arr: list) -> bool:
+    attack_mod_delta_x = (0, 1, 2, 3)
+    attack_mod_delta_y = (0, 1, 2, 3)
 
-        for fig in (old_fig_arr + new_fig_arr):
-            mod_delta_x = abs(fig[0] - x)
-            mod_delta_y = abs(fig[1] - y)
-            if (
+    for fig in (old_fig_arr + new_fig_arr):
+        mod_delta_x = abs(fig[0] - x)
+        mod_delta_y = abs(fig[1] - y)
+        if (
                 mod_delta_x == 1 and mod_delta_y == 1) or (
                 mod_delta_x == 0 and mod_delta_y == 2) or (
                 mod_delta_x == 2 and mod_delta_y == 0) or (
-                    mod_delta_x == 3 and mod_delta_y in [
-                        1, 2, 3]) or (
-                            mod_delta_x in [
-                                1, 2, 3] and mod_delta_y == 3) or (
-                                    mod_delta_x == 2 and mod_delta_y == 2):
-                return False
-            if (mod_delta_x in attack_mod_delta_x
-                    and mod_delta_y in attack_mod_delta_y):
-                return True
-        return False
+                mod_delta_x == 3 and mod_delta_y in [
+            1, 2, 3]) or (
+                mod_delta_x in [
+            1, 2, 3] and mod_delta_y == 3) or (
+                mod_delta_x == 2 and mod_delta_y == 2):
+            return False
+        if (mod_delta_x in attack_mod_delta_x
+                and mod_delta_y in attack_mod_delta_y):
+            return True
+    return False
 
 
-    def cell_is_good(x, y, old_fig_arr, new_fig_arr):
-        return cell_is_free(x, y, old_fig_arr, new_fig_arr) \
-            and not cell_is_attacked(x, y, old_fig_arr, new_fig_arr)
+def cell_is_good(x: int, y: int, old_fig_arr: list, new_fig_arr: list) -> bool:
+    return cell_is_free(x, y, old_fig_arr, new_fig_arr) \
+           and not cell_is_attacked(x, y, old_fig_arr, new_fig_arr)
 
 
-    def get_new_fig_arr(n, new_fig_count, old_fig_arr):
-        new_fig_arr = []
+def get_new_fig_arr(n: int, new_fig_count: int, old_fig_arr: list) -> list:
+    new_fig_arr = []
 
-        for y in range(n):
-            for x in range(n):
+    for y in range(n):
+        for x in range(n):
+            if cell_is_good(x, y, old_fig_arr, new_fig_arr):
+                new_fig_arr.append((x, y))
+
+                if len(new_fig_arr) == new_fig_count:
+                    return new_fig_arr
+    return None
+
+
+def good_move_last_new_fig(n: int, old_fig_arr: list, new_fig_arr: list) -> bool:
+    last_x, last_y = new_fig_arr.pop()
+
+    for y in range(last_y, n):
+        for x in range(n):
+            if y > last_y or x > last_x:
                 if cell_is_good(x, y, old_fig_arr, new_fig_arr):
                     new_fig_arr.append((x, y))
-
-                    if len(new_fig_arr) == new_fig_count:
-                        return new_fig_arr
-        return None
+                    return True
+    return False
 
 
-    def good_move_last_new_fig(n, old_fig_arr, new_fig_arr):
-        last_x, last_y = new_fig_arr.pop()
+def good_complete_new_fig_arr(n: int, new_fig_count: int, old_fig_arr: list, new_fig_arr: list) -> bool:
+    last_x, last_y = new_fig_arr[-1]
+    added_fig_arr = []
 
-        for y in range(last_y, n):
-            for x in range(n):
-                if y > last_y or x > last_x:
-                    if cell_is_good(x, y, old_fig_arr, new_fig_arr):
-                        new_fig_arr.append((x, y))
+    for y in range(last_y, n):
+        for x in range(n):
+            if y > last_y or x > last_x:
+                if cell_is_good(
+                        x,
+                        y,
+                        old_fig_arr,
+                        new_fig_arr +
+                        added_fig_arr):
+                    added_fig_arr.append((x, y))
+                    if len(new_fig_arr + added_fig_arr) == new_fig_count:
+                        new_fig_arr += added_fig_arr
                         return True
-        return False
+    return False
 
 
-    def good_complete_new_fig_arr(n, new_fig_count, old_fig_arr, new_fig_arr):
-        last_x, last_y = new_fig_arr[-1]
-        added_fig_arr = []
+def good_move_new_figures(n: int, new_fig_count: int, old_fig_arr: list, new_fig_arr: list) -> bool:
+    while True:
+        while not good_move_last_new_fig(n, old_fig_arr, new_fig_arr):
+            if len(new_fig_arr) == 0:
+                return False
 
-        for y in range(last_y, n):
-            for x in range(n):
-                if y > last_y or x > last_x:
-                    if cell_is_good(
-                            x,
-                            y,
-                            old_fig_arr,
-                            new_fig_arr +
-                            added_fig_arr):
-                        added_fig_arr.append((x, y))
-                        if len(new_fig_arr + added_fig_arr) == new_fig_count:
-                            new_fig_arr += added_fig_arr
-                            return True
-        return False
+        if len(new_fig_arr) == new_fig_count or good_complete_new_fig_arr(
+                n, new_fig_count, old_fig_arr, new_fig_arr):
+            return True
 
 
-    def good_move_new_figures(n, new_fig_count, old_fig_arr, new_fig_arr):
-        while True:
-            while not good_move_last_new_fig(n, old_fig_arr, new_fig_arr):
-                if len(new_fig_arr) == 0:
-                    return False
+def count_variants(n: int, new_fig_count: int, old_fig_arr: list) -> int:
+    new_list = []
+    res = 0
+    new_fig_arr = get_new_fig_arr(n, new_fig_count, old_fig_arr)
 
-            if len(new_fig_arr) == new_fig_count or good_complete_new_fig_arr(
-                    n, new_fig_count, old_fig_arr, new_fig_arr):
-                return True
+    if new_fig_arr:
 
-
-    def count_variants(n, new_fig_count, old_fig_arr):
-        new_list = []
-        res = 0
-        new_fig_arr = get_new_fig_arr(n, new_fig_count, old_fig_arr)
-
-        if new_fig_arr:
+        res += 1
+        while good_move_new_figures(
+                n, new_fig_count, old_fig_arr, new_fig_arr):
 
             res += 1
-            while good_move_new_figures(
-                    n, new_fig_count, old_fig_arr, new_fig_arr):
 
-                res += 1
-
-                for line in (old_fig_arr + new_fig_arr):
-                    new_list.append('(' + str(line[0]) + ',' + str(line[0]) + '),')
-                new_list.append('\n')
-        if res == 0:
-            new_list.append('No solution')
-        with open(r"output.txt", "w") as file:
-            for line in (new_list):
-                file.write(line)
-            file.close()
-        return res
+            for line in (old_fig_arr + new_fig_arr):
+                new_list.append('(' + str(line[0]) + ',' + str(line[0]) + '),')
+            new_list.append('\n')
+    if res == 0:
+        new_list.append('No solution')
+    with open(r"output.txt", "w") as file:
+        for line in (new_list):
+            file.write(line)
+        file.close()
+    return res
 
 
+'''Получаем переменные из файла'''
+
+
+def get_data_from_file(filename: str) -> tuple:
     flag = True
     old_fig_arr = []
-    with open("p2.txt", "r") as file1:
+    with open(filename, "r") as file1:
         for line in file1:
 
             if flag:
@@ -130,8 +133,12 @@ def main():
                 a = line.strip().split(' ')
                 a = [int(item) for item in a]
                 old_fig_arr.append(tuple(a))
-
         file1.close()
+    return n, l, k,old_fig_arr
+
+
+def main():
+    n, l, k,old_fig_arr = get_data_from_file("p2.txt")
     print(n, l, k, old_fig_arr)
 
     print("n = ", n)
@@ -141,5 +148,7 @@ def main():
     res = count_variants(n, l, old_fig_arr)
     print(res)
     print("--- %s seconds ---" % (time.time() - start_time))
+
+
 if __name__ == "__main__":
     main()
