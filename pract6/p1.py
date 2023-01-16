840ce100a236969726d0fa7b9a29b20bb8798021
MikhailLosev <115175824+MikhailLosev@users.noreply.github.com>
2023-01-16 11:18:34 +0300

    Update p1.py

--- pract6/p1.py
+++ pract6/p1.py
@@ -1,14 +1,16 @@
-def main():
-    def santa_users(lis1):
+def santa_users(lis1:list)->dict:
         dict1 = {}
         for i in lis1:
             if len(i) == 1:
                 dict1[i[0]] = None
             else:
                 dict1[i[0]] = i[1]
-        print(dict1)
+        return dict1
+
+def main():
     lis1 = [["name1 surname2",12345],["name2 surname2"],["name3 surname3",12354],["name4 surname4",12435]]
-    santa_users(lis1)
+    print(santa_users(lis1))
+
 if __name__ == "__main__":
     main()
 
