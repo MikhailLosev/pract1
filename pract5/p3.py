92330fa5c37cfda1244038180c94668b9d6d2d0e
MikhailLosev <115175824+MikhailLosev@users.noreply.github.com>
2023-01-16 21:12:17 +0300

    Update p3.py

--- pract5/p3.py
+++ pract5/p3.py
@@ -1,17 +1,22 @@
-def candies(n:int, k:int)->int:
-        if k > n:
-            return "No solution"
+def candies(n: int, k: int) -> int:
+    if k > n:
+        return "No solution"
 
-        dp = [[1] * n for a in range(n)]
+    dp = [[1] * n for a in range(n)]
 
-        for c in range(2, n):
-            for b in range(1,min(c,k)):
-                dp[c][b] = dp[c-1][b-1] + dp[c-1][b] * (b+1)
-        return dp[n-1][k-1]
+    for c in range(2, n):
+        for b in range(1, min(c, k)):
+            dp[c][b] = dp[c - 1][b - 1] + dp[c - 1][b] * (b + 1)
+    return dp[n - 1][k - 1]
+
 
 def main():
-    print(candies(4,7))
-    print(candies(15,9))
+    n = int(input())
+    k = int(input())
+    print(candies(n, k))
+    #print(candies(4, 7))
+    #print(candies(15, 9))
+
 
 if __name__ == "__main__":
     main()
