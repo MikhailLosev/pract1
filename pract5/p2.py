2a3b6c5cfdff22c3d71bdb17487458d01330873d
MikhailLosev <115175824+MikhailLosev@users.noreply.github.com>
2023-01-16 21:09:43 +0300

    Update p2.py

--- pract5/p2.py
+++ pract5/p2.py
@@ -1,20 +1,25 @@
-def permutList(l:list)->list:
-        if not l:
-                return [[]]
-        res = []
-        for e in l:
-                temp = l[:]
-                temp.remove(e)
-                res.extend([[e] + r for r in permutList(temp)])
-        return res
+def permutList(l: list) -> list:
+    if not l:
+        return [[]]
+    res = []
+    for e in l:
+        temp = l[:]
+        temp.remove(e)
+        res.extend([[e] + r for r in permutList(temp)])
+    return res
+
 
 def main():
-    data = [1,1,2]
+    data = str(input())# 1 1 2
+    data = data.strip()
+    data = data.split()
+    data = [int(x) for x in data]
     new = []
     for i in permutList(data):
         if i not in new:
             new.append(i)
     print(new)
-    
+
+
 if __name__ == "__main__":
     main()
