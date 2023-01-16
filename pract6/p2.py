3abbce93c35233a37732700c3f7ec8be9bdd0b64
MikhailLosev <115175824+MikhailLosev@users.noreply.github.com>
2023-01-16 11:19:25 +0300

    Update p2.py

--- pract6/p2.py
+++ pract6/p2.py
@@ -1,16 +1,15 @@
-def main():
-    def func(str):
-        print(str)
-        integers = dict(I=1, V=5, X=10, L=50, C=100, D=500, M=1000)
-        res = 0
-        for i,val in enumerate(str):
-            if i + 1 < len(str) and integers[str[i]] < integers[str[i + 1]] :
-                res -= integers[str[i]]
-            else:
-                res += integers[str[i]]
-        print(res)
-    func('IV')
-    func('IX')
-if __name__ == "__main__":
-    main()
+def main(str:str)->int:
+    print(str)
+    integers = dict(I=1, V=5, X=10, L=50, C=100, D=500, M=1000)
+    res = 0
+    for i,val in enumerate(str):
+        if i + 1 < len(str) and integers[str[i]] < integers[str[i + 1]] :
+            res -= integers[str[i]]
+        else:
+            res += integers[str[i]]
+    return res
 
+
+if __name__ == "__main__":
+    print(main('IV'))
+    print(main('IX'))
