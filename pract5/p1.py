2f873cbfc5825811d1c23c093e0209251a709e49
MikhailLosev <115175824+MikhailLosev@users.noreply.github.com>
2023-01-16 21:08:02 +0300

    Update p1.py

--- pract5/p1.py
+++ pract5/p1.py
@@ -1,5 +1,13 @@
 def main():
-    list1, list2 = [0, 33, 37, 6, 10, 44, 13, 47, 16, 18, 22, 25], [1, 38, 48, 8, 41, 7, 12, 47, 16, 40, 20, 23, 25]
+    list1 = str(input())# 0 33 37 6 10 44 13 47 16 18 22 25
+    list1 = list1.strip()
+    list1 = list1.split()
+    list1 = [int(x) for x in list1]
+    
+    list2 = str(input()) # 1 38 48 8 41 7 12 47 16 40 20 23 25
+    list2 = list2.strip()
+    list2 = list2.split()
+    list2 = [int(x) for x in list2]
     ans_1 = []
     ans_2 = []
     list1_1 = list1.copy()
@@ -24,3 +32,4 @@ def main():
     print(ans_1, ans_2, ans_3, ans_4)
 if __name__ == "__main__":
     main()
+
