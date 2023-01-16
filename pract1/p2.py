02e273512f79b690337368e995ba224fe51fd449
MikhailLosev <115175824+MikhailLosev@users.noreply.github.com>
2023-01-16 10:59:24 +0300

    Update p2.py

--- pract1/p2.py
+++ pract1/p2.py
@@ -1,10 +1,15 @@
 def main():
-    s = int(input())
-    s2 = str(s)
-    r = s2[:-2:]
-    if -2**7 <= s <= 2**7 - 1:
-        print(r)
+    s = input()
+    s2 = ""
+    for i in range(len(s)-1,-1,-1):
+        s2 = s2 + s[i]
+    if int(s) <0:
+        s2 = '-'+ s2[:len(s2)-1]
+
+    if -128<int(s2)<127:
+        print(int(s2))
     else:
         print("no solution")
 if __name__ == "__main__":
     main()
+
